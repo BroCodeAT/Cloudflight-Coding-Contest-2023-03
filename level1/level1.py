5292a2155adde29bd33f7887eb37ccce0c317c48
kloaChristoph <christoph.alexander.holas@gmail.com>
2023-03-31 15:09:00 +0200

    check_winner

--- level1/level1.py
+++ level1/level1.py
@@ -3,3 +3,13 @@ def load_file(filename: str = "level1.in") -> list:
     with open(filename, "r") as file:
         return [line.strip() for line in file.readlines()]
     
+def check_winner(styles: str) -> str:
+    if "R" in styles and "P" in styles:
+        return "P"
+    elif "R" in styles and "S" in styles:
+        return "R"
+    elif "P" in styles and "S" in styles:
+        return "S"
+    elif styles[0] == styles[1]:
+        return styles[0]
+    
