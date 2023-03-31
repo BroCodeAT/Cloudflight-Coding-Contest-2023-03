7c9985bfcb241022b3bb52d00bf565206c013f65
Daniel Pfurner <dpfurner@tsn.at>
2023-03-31 15:30:52 +0200

    Level2: Daniel done

--- level2/level2.py
+++ level2/level2.py
@@ -13,12 +13,12 @@ def write_to_file(data: list, filename: str = "level1/level1_1.out") -> None:
 def tournament(data: list) -> list:
     winner = []
     for tourn in data[1:]:
-        if win := check_winner(tourn):
+        if win := check_tournament_winners(tourn):
             winner.append(win)
     return winner
 
 
 if __name__ == '__main__':
-    for i in range(1, 6):
-        comp = competition(load_file(f"level1/level1_{i}.in"))
-        write_to_file(comp, f"level1/level1_{i}.out")
+    for i in range(1, 5):
+        comp = tournament(load_file(f"level2/level2_{i}.in"))
+        write_to_file(comp, f"level2/level2_{i}.out")
