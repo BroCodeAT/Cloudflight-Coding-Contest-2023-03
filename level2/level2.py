ec8f009ad32b022c95046376c3b2427833e1a90a
Daniel Pfurner <dpfurner@tsn.at>
2023-03-31 15:21:47 +0200

    Level2 Setup

--- level2/level2.py
+++ level2/level2.py
@@ -1,6 +1,17 @@
 
-def load_file(filename: str = "level2.in") -> list:
+def load_file(filename: str = "level1/level1_1.in") -> list:
     with open(filename, "r") as file:
         return [line.strip() for line in file.readlines()]
+
+
+def write_to_file(data: list, filename: str = "level1/level1_1.out") -> None:
+    with open(filename, "w") as file:
+        for line in data:
+            file.write(line + "\n")
     
-    
+
+
+if __name__ == '__main__':
+    for i in range(1, 6):
+        comp = competition(load_file(f"level1/level1_{i}.in"))
+        write_to_file(comp, f"level1/level1_{i}.out")
